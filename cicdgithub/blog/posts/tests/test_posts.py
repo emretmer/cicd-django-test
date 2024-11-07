99570deb5298490f709586e49de61f00584b78d0
Ahmet Emre Tümer <aemret999@gmail.com>
2024-11-07 17:33:21 +0300

    s

--- cicdgithub/blog/posts/tests/test_posts.py
+++ cicdgithub/blog/posts/tests/test_posts.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from posts.models import Post  # Correct absolute import
 
-
 class PostModelTest(TestCase):
     def test_create_post(self):
         post = Post.objects.create(title="Test Title", content="Test Content")
