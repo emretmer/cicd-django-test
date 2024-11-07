43e261da9e46b61b81836333c5b723d2b25d3f7d
Ahmet Emre Tümer <aemret999@gmail.com>
2024-11-07 12:37:40 +0300

    pytest django

--- cicdgithub/blog/manage.py
+++ cicdgithub/blog/manage.py
@@ -2,11 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import django
 
 def main():
     """Run administrative tasks."""
+    # Django ayarlarını doğru şekilde yüklüyoruz
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+    
+    # Django'yu başlatıyoruz
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +18,12 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+    # Django'yu başlatmadan önce, testlerde ihtiyaç duyulan ayarları yüklemek için django.setup() ekleyebilirsiniz
+    django.setup()
+
+    # Komutları çalıştır
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
